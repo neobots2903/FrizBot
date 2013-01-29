16a9355c509075ff82baaf14fe7dcc11d0343536
Zach Ensey <endlesszord@gmail.com>
2013-01-29 15:56:19 -0800

    Added Shooter.java

---
+++ src/edu/first/team2903/robot/subsystems/Shooter.java
@@ -0,0 +1,17 @@
+package edu.first.team2903.robot.subsystems;
+
+import edu.first.team2903.robot.RobotMap;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Shooter extends Subsystem {
+
+    Jaguar shooterWheel = new Jaguar (RobotMap.shooterWheel);
+            
+    protected void initDefaultCommand() {
+    }
+    
+    public void Shoot(double speed) {
+        shooterWheel.set(speed);
+    }
+}
