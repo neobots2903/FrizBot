b8504cd9b131d5c05f65f0eb988b417d7861f94a
Zach Ensey <endlesszord@gmail.com>
2013-01-29 16:08:13 -0800

    Removed comments

--- src/edu/first/team2903/robot/subsystems/Camera.java
+++ src/edu/first/team2903/robot/subsystems/Camera.java
@@ -1,15 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.first.team2903.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- * @author robotics
- */
 public class Camera  extends Subsystem {
 
     protected void initDefaultCommand() {
