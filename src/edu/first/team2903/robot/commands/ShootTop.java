81aa3aa7afef1bde90591ac4c9643e3dbca057b1
Zach Ensey <endlesszord@gmail.com>
2013-01-30 17:17:41 -0800

    Added ShootTop.java

---
+++ src/edu/first/team2903/robot/commands/ShootTop.java
@@ -0,0 +1,72 @@
+package edu.first.team2903.robot.commands;
+//TODO 
+
+import edu.first.team2903.robot.PerodicDataFactory;
+import edu.wpi.first.wpilibj.image.NIVisionException;
+import edu.wpi.first.wpilibj.networktables2.thread.PeriodicRunnable;
+
+public class ShootTop extends CommandBase{
+
+    public ShootTop() {
+    requires(shooter);
+    requires(camera);
+    }
+
+    
+    protected void initialize() {
+    }
+
+    protected void execute() {
+        try {
+            boolean fired = false;
+            /*??check if the disk is loaded??
+            *-IF LOADED-
+            *Get the distance from the target
+            *Turn it into RPM target
+            *turn on motor until it reaches ^ RPM
+            * (If > RPM +15){
+            * lower or stop motor power
+            * }
+            *  (if < RPM -15){
+            * Speed up motor
+            * }
+            *fHIER
+            *-IF NOT LOADED-
+            *Stop Running command
+            *print error message to driver station(DISK NOT LOADED!) */
+                //if(diskLoaded = true) {
+                  double topDistance = camera.computeTopDistance();
+                    if(topDistance == -1) {
+                        System.out.println("No top target detected.");
+                    }
+                    else {
+                        double rpm = PerodicDataFactory.getInstance().getRPM();
+                        double targetrpm = 9001;
+                        while(fired == false) {
+                        if(rpm > (targetrpm - 15)&& rpm < (targetrpm +15)){
+                            //make it fHIER!(need to make whatever pushes the frisbee power on.)
+                            fired = true;
+                        }
+                        }
+                    }
+                //}
+                //else(){
+                //System.out.println("Disk is not loaded!");
+                //}
+        } catch (NIVisionException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
+    protected boolean isFinished() {
+    return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+    
+}
