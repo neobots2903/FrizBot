391c770e81acdd1a4ff581c593b0a3346635c915
Zach Ensey <endlesszord@gmail.com>
2013-02-01 16:54:17 -0800

    Added joystick subsystem.

---
+++ src/edu/first/team2903/robot/subsystems/Joysticks.java
@@ -0,0 +1,19 @@
+package edu.first.team2903.robot.subsystems;
+
+import edu.first.team2903.robot.RobotMap;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Joysticks extends Subsystem {
+
+    Joystick leftJoy = new Joystick(1);
+    Joystick rightJoy = new Joystick(2);
+    
+    
+    protected void initDefaultCommand() {
+    }
+    
+    public void Joystick() {
+    }
+}
