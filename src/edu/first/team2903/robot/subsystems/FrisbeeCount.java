2ae1c19bd827d2e42c9b2168318cedf481182ce4
Gavin Britton <gavin.britton@gmail.com>
2013-02-02 13:15:55 -0800

    Added the "FrisbeeCount.java" all pseudocode at this point

---
+++ src/edu/first/team2903/robot/subsystems/FrisbeeCount.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.first.team2903.robot.subsystems;
+
+/**
+ *
+ * @author robotics
+ */
+public class FrisbeeCount {
+    
+    //if (FCsensor1 = true) {frisbeeCount++}
+    // if (FCsensor2 = true) {frisbeeCount--}
+    
+}
