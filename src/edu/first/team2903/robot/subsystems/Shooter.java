8f1a3aed329a15a941e7d10429f711e10d724589
Zach Ensey <endlesszord@gmail.com>
2013-02-01 17:41:53 -0800

    Changed the frisbee pusher to a relay.

--- src/edu/first/team2903/robot/subsystems/Shooter.java
+++ src/edu/first/team2903/robot/subsystems/Shooter.java
@@ -2,12 +2,13 @@ package edu.first.team2903.robot.subsystems;
 
 import edu.first.team2903.robot.RobotMap;
 import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Shooter extends Subsystem {
 
     Jaguar shooterWheel = new Jaguar (RobotMap.shooterWheel);
-    Jaguar frisbeePusher = new Jaguar (RobotMap.shooterWheel);
+    Relay frisbeePusher = new Relay (RobotMap.frisbeePusher);
             
     protected void initDefaultCommand() {
     }
